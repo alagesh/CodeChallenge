7a176c41f51acd32f897ce97bba0d71708dc36e1
Azhakesan Thangamuthu <10376513+alagesh@users.noreply.github.com>
2018-02-02 12:55:21 -0800

    BDD Mockito Test cases :Controller & Business Impl

--- src/test/java/com/sandbox/Challenge/controller/LaunchControllerTest.java
+++ src/test/java/com/sandbox/Challenge/controller/LaunchControllerTest.java
@@ -24,6 +24,9 @@ public class LaunchControllerTest {
 	@InjectMocks
 	LaunchController controller;
 	
+	/*
+	 * Test case :  givenInputDateRange_WhenGetAllValidLaunchDates_thenReturnsEmpty
+	 */
 	@Test
 	public void givenInputDateRange_WhenGetAllValidLaunchDates_thenReturnsEmpty() {
 		String start_date="2015-09-07";
@@ -42,5 +45,50 @@ public class LaunchControllerTest {
 		assertEquals(0,emptyReturn.size());
 		
 	}
+	
+	/*
+	 * Test Case: givenInputDateRange_WhenGetAllValidLaunchDates_thenReturns2Dates
+	 */
+	
+	@Test
+	public void givenInputDateRange_WhenGetAllValidLaunchDates_thenReturns2Dates() {
+		String start_date="2015-09-07";
+		String end_date="2015-09-13";
+		
+		List<String> dataReturn =Arrays.asList("2015-09-07","2015-09-08");
+		
+		//Given
+		given(businessService.getValidLaunchDates(start_date, end_date))
+			.willReturn(dataReturn);
+		
+		//When
+		List<String> returnDates = controller.getValidLaunchDates(start_date, end_date);
+		
+		//Then
+		assertEquals(2,dataReturn.size());
+		
+	}
+	
+	/*
+	 * Test Case: givenInputDateRange_WhenGetAllValidLaunchDates_thenThrowsException
+	 */
+	
+	@Test(expected=RuntimeException.class)
+	public void givenInputDateRange_WhenGetAllValidLaunchDates_thenThrowsException() {
+		String start_date="2015-09-07";
+		String end_date="2015-09-13";
+		
+		//Given
+		given(businessService.getValidLaunchDates(start_date, end_date))
+			.willThrow(new RuntimeException());
+		
+		//When
+		List<String> returnDates = controller.getValidLaunchDates(start_date, end_date);
+		
+		//Then
+		
+		
+	}
+	
 
 }
--- src/test/java/com/sandbox/Challenge/services/BusinessServiceImplTest.java
+++ src/test/java/com/sandbox/Challenge/services/BusinessServiceImplTest.java
@@ -1,10 +1,20 @@
 package com.sandbox.Challenge.services;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.BDDMockito.given;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.sandbox.Challenge.model.NearEarthObject;
 import com.sandbox.Challenge.services.Impl.BusinessServiceImpl;
 
 
@@ -15,6 +25,46 @@ public class BusinessServiceImplTest {
 	ApiService apiService;
 
 	@InjectMocks
-	BusinessServiceImpl service;
+	BusinessServiceImpl serviceImpl;
 
+	
+	/*
+	 * Test case :  givenInputDateRange_WhenGetAllValidLaunchDates_thenReturnsEmpty
+	 */
+	@Test
+	public void givenInputDateRange_WhenGetAllValidLaunchDates_thenReturnsEmpty() {
+		String start_date="2015-09-07";
+		String end_date="2015-09-13";
+		
+		//Given
+		given(apiService.getAllNeoForDateRange(start_date, end_date))
+			.willReturn(null);
+		
+		//When
+		Map<String, List<NearEarthObject>>  actualReturn  = apiService.getAllNeoForDateRange(start_date, end_date);
+		
+		//Then
+		assertNull(actualReturn);
+		
+	}
+	
+	/*
+	 * Test case :  givenInputDateRange_WhenGetAllValidLaunchDates_thenThrowsException
+	 */
+	@Test(expected=RuntimeException.class)
+	public void givenInputDateRange_WhenGetAllValidLaunchDates_thenThrowsException() {
+		String start_date="2015-09-07";
+		String end_date="2015-09-13";
+		
+		//Given
+		given(apiService.getAllNeoForDateRange(start_date, end_date))
+		.willThrow(new RuntimeException());
+		
+		//When
+		Map<String, List<NearEarthObject>>  actualReturn  = apiService.getAllNeoForDateRange(start_date, end_date);
+		
+		//Then
+		
+		
+	}
 }
