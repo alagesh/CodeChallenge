0856edcbdf9b7bf473dc1c0a6fdb22f624bfd69a
Azhakesan Thangamuthu <10376513+alagesh@users.noreply.github.com>
2018-02-02 12:56:21 -0800

    Delete ApiServiceImplTest.java

--- src/test/java/com/sandbox/Challenge/services/ApiServiceImplTest.java
+++
@@ -1,9 +0,0 @@
-package com.sandbox.Challenge.services;
-
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-public class ApiServiceImplTest {
-
-}
